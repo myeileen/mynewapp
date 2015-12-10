d32375c3c7553d087f196399cdf7bcd10537741a
myeileen <eileen@myeileen.com>
2015-12-09 17:43:19 -0800

    add carousel and partial to products

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,6 @@
 class StaticPagesController < ApplicationController
-  def index
-  end
 
   def landing_page
-    @featured_product = Product.first
-  end  
+    @products = Product.limit(3)
+  end
 end
